5ef5969295c1d2bf1aaee1f325b9da3b8a69a5c7
saikrishna-ql <saikrishna@quantixlabs.ai>
2025-11-04 17:12:11 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,8 @@
-def call(String url, String branch){
-  git url: "${url}", branch: "${branch}"
+def call(String url, String branch, String credentialsId = '') {
+    if (credentialsId?.trim()) {
+        git branch: branch, url: url, credentialsId: credentialsId
+    } else {
+        git branch: branch, url: url
+    }
 }
+
